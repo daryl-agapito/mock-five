a72af2cceb7c36c3f549d7796ddde90ef4d8bb71
daryl-agapito <62405189+daryl-agapito@users.noreply.github.com>
2024-05-07 18:05:46 -0700

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-7xl font-bold text-blue-500">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
